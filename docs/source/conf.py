d2a0f98e946494e59ca53d557d046b6df1f9d905
Kevin Lane <lane.kevin.a@gmail.com>
2021-04-11 13:08:14 -0700

    Remove duplicate variable in Sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -82,8 +82,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-templates_path = ["_templates"]
-
 
 html_css_files = [
     "css/custom.css",
