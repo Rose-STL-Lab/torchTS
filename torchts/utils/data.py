a2a835db292b9a4e0c3498236d61a24ce57b9185
Kevin Lane <lane.kevin.a@gmail.com>
2021-04-17 13:32:03 -0700

    Allow lagmat to accept an iterable of lags

--- torchts/utils/data.py
+++ torchts/utils/data.py
@@ -1,3 +1,12 @@
+from collections.abc import Iterable
+
+
 def lagmat(tensor, lags, horizon=1, dim=0, step=1):
-    data = tensor.unfold(dim, lags + horizon, step)
-    return data[:, :lags], data[:, -1]
+    if isinstance(lags, Iterable):
+        data = tensor.unfold(dim, max(lags) + horizon, step)
+        x, y = data[:, [lag - 1 for lag in lags]], data[:, -1]
+    else:
+        data = tensor.unfold(dim, lags + horizon, step)
+        x, y = data[:, :lags], data[:, -1]
+
+    return x, y
