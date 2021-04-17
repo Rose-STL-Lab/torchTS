fe6ad4593831f1343c2d379ebb98f173a08f6f38
Kevin Lane <lane.kevin.a@gmail.com>
2021-04-17 14:06:08 -0700

    Ensure lags contain integers

--- torchts/utils/data.py
+++ torchts/utils/data.py
@@ -2,11 +2,13 @@ from collections.abc import Iterable
 
 
 def lagmat(tensor, lags, horizon=1, dim=0, step=1):
-    if isinstance(lags, Iterable):
+    if isinstance(lags, int):
+        data = tensor.unfold(dim, lags + horizon, step)
+        x, y = data[:, :lags], data[:, -1]
+    elif isinstance(lags, Iterable) and all(isinstance(lag, int) for lag in lags):
         data = tensor.unfold(dim, max(lags) + horizon, step)
         x, y = data[:, [lag - 1 for lag in lags]], data[:, -1]
     else:
-        data = tensor.unfold(dim, lags + horizon, step)
-        x, y = data[:, :lags], data[:, -1]
+        raise TypeError("lags must be of type int or Iterable[int]")
 
     return x, y
