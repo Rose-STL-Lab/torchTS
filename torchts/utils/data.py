168b316ad958f8c548e559e2e4f2eb57771366cb
Kevin Lane <lane.kevin.a@gmail.com>
2021-04-17 14:39:41 -0700

    Refactor lagmat

--- torchts/utils/data.py
+++ torchts/utils/data.py
@@ -2,13 +2,17 @@ from collections.abc import Iterable
 
 
 def lagmat(tensor, lags, horizon=1, dim=0, step=1):
-    if isinstance(lags, int):
+    is_int = isinstance(lags, int)
+    is_iter = isinstance(lags, Iterable) and all(isinstance(lag, int) for lag in lags)
+
+    if not is_int and not is_iter:
+        raise TypeError("lags must be of type int or Iterable[int]")
+
+    if is_int:
         data = tensor.unfold(dim, lags + horizon, step)
         x, y = data[:, :lags], data[:, -1]
-    elif isinstance(lags, Iterable) and all(isinstance(lag, int) for lag in lags):
+    else:
         data = tensor.unfold(dim, max(lags) + horizon, step)
         x, y = data[:, [lag - 1 for lag in lags]], data[:, -1]
-    else:
-        raise TypeError("lags must be of type int or Iterable[int]")
 
     return x, y
