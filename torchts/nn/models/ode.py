581efa0e751c1406835634d8add25d407671b2ee
amartyamukherjee <am4521ty@gmail.com>
2021-12-19 20:50:46 -0500

    Changed ODESolver back to klane's model

--- torchts/nn/models/ode.py
+++ torchts/nn/models/ode.py
@@ -14,9 +14,7 @@ class ODESolver(TimeSeriesModel):
             raise ValueError("Inconsistent keys in ode and init_vars")
 
         if solver == "euler":
-            self.step_solver = self.euler_step
-        elif solver == "rk4":
-            self.step_solver = self.runge_kutta_4_step
+            self.solver = self.euler
         else:
             raise ValueError(f"Unrecognized solver {solver}")
 
@@ -33,51 +31,16 @@ class ODESolver(TimeSeriesModel):
         self.outvar = self.var_names if outvar is None else outvar
         self.dt = dt
 
-    def euler_step(self, prev_val):
+    def euler(self, nt):
         pred = {name: value.unsqueeze(0) for name, value in self.init_vars.items()}
-        for var in self.var_names:
-            pred[var] = prev_val[var] + self.ode[var](prev_val, self.coeffs) * self.dt
-        return pred
-
-    def runge_kutta_4_step(self, prev_val):
-        pred = {name: value.unsqueeze(0) for name, value in self.init_vars.items()}
-
-        k_1 = prev_val
-        k_2 = {}
-        k_3 = {}
-        k_4 = {}
-
-        for var in self.var_names:
-            k_2[var] = (
-                prev_val[var] + self.ode[var](prev_val, self.coeffs) * 0.5 * self.dt
-            )
-
-        for var in self.var_names:
-            k_3[var] = prev_val[var] + self.ode[var](k_2, self.coeffs) * 0.5 * self.dt
-
-        for var in self.var_names:
-            k_4[var] = prev_val[var] + self.ode[var](k_3, self.coeffs) * self.dt
-
-        for var in self.var_names:
-            result = self.ode[var](k_1, self.coeffs) / 6
-            result += self.ode[var](k_2, self.coeffs) / 3
-            result += self.ode[var](k_3, self.coeffs) / 3
-            result += self.ode[var](k_4, self.coeffs) / 6
-            pred[var] = prev_val[var] + result * self.dt
-
-        return pred
-
-    def solver(self, nt, initial=None):
-        if initial is None:
-            initial = self.init_vars
-        pred = {name: value.unsqueeze(0) for name, value in initial.items()}
 
         for n in range(nt - 1):
             # create dictionary containing values from previous time step
             prev_val = {var: pred[var][[n]] for var in self.var_names}
-            new_val = self.step_solver(prev_val)
+
             for var in self.var_names:
-                pred[var] = torch.cat([pred[var], new_val[var]])
+                new_val = prev_val[var] + self.ode[var](prev_val, self.coeffs) * self.dt
+                pred[var] = torch.cat([pred[var], new_val])
 
         # reformat output to contain desired (observed) variables
         return torch.stack([pred[var] for var in self.outvar], dim=1)
@@ -89,17 +52,7 @@ class ODESolver(TimeSeriesModel):
         return {name: param.item() for name, param in self.named_parameters()}
 
     def _step(self, batch, batch_idx, num_batches):
-        x, y = self.prepare_batch(batch)
-        nt = y.shape[1]
-
-        init_point = {var: x[0, i] for i, var in enumerate(self.var_names)}
-        pred = self.solver(nt, init_point)
-
-        # TODO: Account for batch size > 1
-        pred = pred.view(1, pred.shape[0], pred.shape[1])
-
-        if self.criterion_args is not None:
-            loss = self.criterion(pred, y, **self.criterion_args)
-        else:
-            loss = self.criterion(pred, y)
-        return loss
+        (x,) = batch
+        nt = x.shape[0]
+        pred = self(nt)
+        return self.criterion(pred, x)
