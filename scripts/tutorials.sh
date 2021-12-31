83acf09a46539a089edd2596ed8aa620a2a6f381
Aadit Gupta <spacegiant21@gmail.com>
2021-12-30 20:51:02 -0800

    add comments in scripts

--- scripts/tutorials.sh
+++ scripts/tutorials.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
 for i in $(find ../examples -name '*.ipynb'); do
+
+    # Converting notebook to markdown
     jupyter nbconvert --to markdown $i
+
+    # Removing ipynb extension from file path
     modified=${i::-6}
     echo $modified
+
+    # Setting variable for folder which contains images
     files="${modified}_files"
     echo $files
+
+    # Extracts just the filename from large path variable
     out="$(basename $files)"
     echo $out
+
+    # Fixes images path
     sed -i "s/$out/\/img/g" "$modified.md"
+
+    #Moves images into static folder of website
     mv $files/* ../website/static/img
+
+    # Moves Markdown file into tutorial folder
     mv "$modified.md" ../website/tutorials
 done
