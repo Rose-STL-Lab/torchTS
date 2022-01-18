90e27ff76f44c8f330042796d26c48c1d6c9506a
amartyamukherjee <am4521ty@gmail.com>
2022-01-18 02:35:05 -0500

    Update test_ode.py

--- tests/nn/test_ode.py
+++ tests/nn/test_ode.py
@@ -23,6 +23,24 @@ def euler_model():
 
 
 @pytest.fixture
+def euler_unobserved_model():
+    # ODE: x'(t) = 2x
+    model = ODESolver(
+        {"x": lambda prev_val, coeffs: coeffs["alpha"] * prev_val["x"],
+        "y": lambda prev_val, coeffs: prev_val["x"]},
+        {"x": 1.0,"y":1.0},
+        {"alpha": 2.0},
+        0.1,
+        solver="euler",
+        outvar=["x"],
+        optimizer=torch.optim.Adam,
+        optimizer_args={"lr": 0.5},
+    )
+    preds = model(2)
+    return model, preds
+
+
+@pytest.fixture
 def rk4_model():
     # ODE: x'(t) = x
     model = ODESolver(
@@ -63,6 +81,7 @@ def test_generate_ode_dataset(euler_model):
 
 
 def test_value_errors():
+    """Detects ValueError in the __init__ function"""
     with pytest.raises(ValueError, match="Unrecognized solver .*"):
         model = ODESolver(
             {"x": lambda prev_val, coeffs: coeffs["alpha"] * prev_val["x"]},
@@ -85,6 +104,7 @@ def test_value_errors():
 
 
 def test_step_backward(euler_model):
+    """Test the step and backward function"""
     torch.manual_seed(0)
     batch = torch.Tensor([[1.0]]), torch.Tensor([[1.1]])
     model, preds = euler_model
@@ -97,8 +117,31 @@ def test_step_backward(euler_model):
 
 
 def test_fit(euler_model):
+    """Test the step and backward function"""
     torch.manual_seed(0)
     model, preds = euler_model
     model.fit(torch.Tensor([[1.0]]), torch.Tensor([[1.1]]), max_epochs=1, batch_size=1)
+    coeffs = model.get_coeffs()
+    assert coeffs["alpha"] < 2
+
+
+def test_unobserved_step_backward(euler_unobserved_model):
+    """Test the step and backward function"""
+    torch.manual_seed(0)
+    batch = torch.Tensor([[1.0,1.1]]), torch.Tensor([[1.1,1.2]])
+    model, preds = euler_unobserved_model
+    loss = model._step(batch, 0, 0)
+    assert (loss.item() - ((1.1 - 1.0) ** 2+(1.2 - 1.1) ** 2)/2) < 1e-6
+    model.backward(loss, None, 0)
+    model.optimizer(model.parameters()).step()
+    coeffs = model.get_coeffs()
+    assert coeffs["alpha"] < 2
+
+
+def test_unobserved_fit(euler_unobserved_model):
+    """Test the step and backward function"""
+    torch.manual_seed(0)
+    model, preds = euler_unobserved_model
+    model.fit(torch.Tensor([[1.0,1.1]]), torch.Tensor([[1.1,1.2]]), max_epochs=1, batch_size=1)
     coeffs = model.get_coeffs()
     assert coeffs["alpha"] < 2
