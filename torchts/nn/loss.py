15e8228b4b4520ac2704029ab45043b054249e4f
judyyy <jinzhao0531@qq.com>
2022-02-11 02:38:21 -0800

    add conformal

--- torchts/nn/loss.py
+++ torchts/nn/loss.py
@@ -1,6 +1,7 @@
 from typing import List, Union
 
 import torch
+import numpy as np
 
 
 def masked_mae_loss(y_pred, y_true):
@@ -48,3 +49,18 @@ def quantile_loss(
     loss = torch.sum(loss)
 
     return loss
+
+def quantile_err(prediction, y):
+    """
+    prediction: arr where first 3 columns are: lower quantile, middle quantile (50%), upper quantile in that order
+    """
+    y_lower = prediction[:, 0]
+    y_upper = prediction[:, 2]
+    # Calculate error on our predicted upper and lower quantiles
+    # this will get us an array of negative values with the distance between the upper/lower quantile and the
+    # 50% quantile
+    error_low = y_lower - y
+    error_high = y - y_upper
+    # Make an array where each entry is the highest error when comparing the upper and lower bounds for that entry prediction
+    err = np.maximum(error_high, error_low)
+    return err
--- torchts/nn/model.py
+++ torchts/nn/model.py
@@ -2,9 +2,10 @@ from abc import abstractmethod
 from functools import partial
 
 import torch.nn.functional as F
+import numpy as np
 from pytorch_lightning import LightningModule, Trainer
 from torch.utils.data import DataLoader, TensorDataset
-
+from torchts.nn.loss import quantile_err
 
 class TimeSeriesModel(LightningModule):
     """Base class for all TorchTS models.
@@ -14,6 +15,7 @@ class TimeSeriesModel(LightningModule):
         opimizer_args (dict): Arguments for the optimizer
         criterion: Loss function
         criterion_args (dict): Arguments for the loss function
+        method: conformal prediction 
         scheduler (torch.optim.lr_scheduler): Learning rate scheduler
         scheduler_args (dict): Arguments for the scheduler
         scaler (torchts.utils.scaler.Scaler): Scaler
@@ -25,6 +27,8 @@ class TimeSeriesModel(LightningModule):
         optimizer_args=None,
         criterion=F.mse_loss,
         criterion_args=None,
+        significance=None,
+        method=None,
         scheduler=None,
         scheduler_args=None,
         scaler=None,
@@ -32,6 +36,8 @@ class TimeSeriesModel(LightningModule):
         super().__init__()
         self.criterion = criterion
         self.criterion_args = criterion_args
+        self.significance = significance
+        self.method = method
         self.scaler = scaler
 
         if optimizer_args is not None:
@@ -55,6 +61,7 @@ class TimeSeriesModel(LightningModule):
         """
         dataset = TensorDataset(x, y)
         loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        # split to only train on training set
         trainer = Trainer(max_epochs=max_epochs)
         trainer.fit(self, loader)
 
@@ -90,6 +97,40 @@ class TimeSeriesModel(LightningModule):
             loss = self.criterion(pred, y)
 
         return loss
+    
+    def calibration(self, batch, batch_idx):
+        """
+
+        Args:
+            batch: Output of the torch.utils.data.DataLoader
+            batch_idx: Integer displaying index of this batch
+            dataset: Data set to use
+
+        Returns: err_dist for the calibration set
+        """
+        x, y = self.prepare_batch(batch)
+        batches_seen = batch_idx
+        pred = self(x, y, batches_seen)
+
+        if self.scaler is not None:
+            y = self.scaler.inverse_transform(y)
+            pred = self.scaler.inverse_transform(pred)
+
+        cal_scores = quantile_err(pred, y)
+
+        nc = {0: np.sort(cal_scores, 0)[::-1]}
+        # significance = .1
+        # Sort calibration scores in ascending order? TODO make sure this is correct
+        # this is the apply_inverse portion of RegressorNC predict function
+        nc = np.sort(cal_scores, 0)
+
+        index = int(np.ceil((1 - self.significance) * (nc.shape[0] + 1))) - 1
+        # find largest error that gets us guaranteed coverage
+        index = min(max(index, 0), nc.shape[0] - 1)
+
+        err_dist = np.vstack([nc[index], nc[index]])
+        
+        return err_dist
 
     def training_step(self, batch, batch_idx):
         """Trains model for one step.
@@ -116,6 +157,7 @@ class TimeSeriesModel(LightningModule):
             batch (torch.Tensor): Output of the torch.utils.data.DataLoader
             batch_idx (int): Integer displaying index of this batch
         """
+        self.cal_score = self.calibration(batch, batch_idx, len(self.trainer.train_dataloader))
         val_loss = self._step(batch, batch_idx, len(self.trainer.val_dataloader))
         self.log("val_loss", val_loss)
         return val_loss
@@ -166,3 +208,47 @@ class TimeSeriesModel(LightningModule):
             return [optimizer], [scheduler]
 
         return optimizer
+
+
+# class TimeSeriesConformalModel(TimeSeriesModel):
+#     def __init__(
+#         self,
+#         optimizer,
+#         optimizer_args=None,
+#         criterion=F.mse_loss,
+#         criterion_args=None,
+#         scheduler=None,
+#         scheduler_args=None,
+#         scaler=None,
+#     ):
+#         super().__init__()
+#         self.criterion = criterion
+#         self.criterion_args = criterion_args
+#         self.scaler = scaler
+
+#         if optimizer_args is not None:
+#             self.optimizer = partial(optimizer, **optimizer_args)
+#         else:
+#             self.optimizer = optimizer
+
+#         if scheduler is not None and scheduler_args is not None:
+#             self.scheduler = partial(scheduler, **scheduler_args)
+#         else:
+#             self.scheduler = scheduler
+    
+#     def predict()
+
+#     def quantile_err(prediction, y):
+#         """
+#         prediction: arr where first 3 columns are: lower quantile, middle quantile (50%), upper quantile in that order
+#         """
+#         y_lower = prediction[:, 0]
+#         y_upper = prediction[:, 2]
+#         # Calculate error on our predicted upper and lower quantiles
+#         # this will get us an array of negative values with the distance between the upper/lower quantile and the
+#         # 50% quantile
+#         error_low = y_lower - y
+#         error_high = y - y_upper
+#         # Make an array where each entry is the highest error when comparing the upper and lower bounds for that entry prediction 
+#         err = np.maximum(error_high, error_low)
+#         return err
