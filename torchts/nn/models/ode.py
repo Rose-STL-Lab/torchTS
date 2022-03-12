80392eb4a8e39d6b16a8e1b79987e020d9b9c3f6
Kevin Lane <lane.kevin.a@gmail.com>
2022-03-12 10:45:16 -0800

    Update comments and format

--- torchts/nn/models/ode.py
+++ torchts/nn/models/ode.py
@@ -8,15 +8,16 @@ class ODESolver(TimeSeriesModel):
     def __init__(
         self, ode, init_vars, init_coeffs, dt, solver="euler", outvar=None, **kwargs
     ):
-        """
+        """TimeSeriesModel for modeling ordinary differential equations.
+
         Args:
-            ode (dict): ODE in dictionary form.
-            init_vars (dict): Initial values for each variable.
-            init_coeffs (dict): Initial values for each parameter.
-            dt (float): Time step.
-            solver ("euler"/"rk4"): Numerical method for solving the ODE.
-            outvar (list): Observed variables.
-            kwargs.optimizer (torch.optim): Optimizer.
+            ode (dict): ODE in dictionary form
+            init_vars (dict): Initial values for each variable
+            init_coeffs (dict): Initial values for each parameter
+            dt (float): Time step
+            solver (str): Numerical method for solving the ODE ("euler"/"rk4")
+            outvar (list): Observed variables
+            kwargs (Any): Additional arguments passed to TimeSeriesModel
         """
         super().__init__(**kwargs)
 
@@ -42,30 +43,35 @@ class ODESolver(TimeSeriesModel):
         self.coeffs = {name: param for name, param in self.named_parameters()}
         self.outvar = self.var_names if outvar is None else outvar
 
-        self.observed = set(self.outvar) == set(
-            self.var_names
-        )  # Figures out method of training
+        # determines method of training
+        self.observed = set(self.outvar) == set(self.var_names)
 
         self.dt = dt
 
     def _euler_step(self, prev_val):
-        """Computes a single Euler's method step for the ODE
+        """Computes a single step of the ODE using Euler's method.
+
         Args:
-            prev_val (dict): Previous values for each variable.
+            prev_val (dict): Previous values for each variable
+
         Returns:
-            pred (dict): Euler's method step prediction.
+            dict: Euler's method step prediction
         """
         pred = {name: value.unsqueeze(0) for name, value in self.init_vars.items()}
+
         for var in self.var_names:
             pred[var] = prev_val[var] + self.ode[var](prev_val, self.coeffs) * self.dt
+
         return pred
 
     def _runge_kutta_4_step(self, prev_val):
-        """Computes a single 4th order Runge-Kutta method step for the ODE
+        """Computes a single step of the ODE using a 4th order Runge-Kutta method.
+
         Args:
-            prev_val (dict): Previous values for each variable.
+            prev_val (dict): Previous values for each variable
+
         Returns:
-            pred (dict): 4th order Runge-Kutta method step prediction.
+            dict: 4th order Runge-Kutta method step prediction
         """
         pred = {name: value.unsqueeze(0) for name, value in self.init_vars.items()}
 
@@ -75,9 +81,7 @@ class ODESolver(TimeSeriesModel):
         k_4 = {}
 
         for var in self.var_names:
-            k_2[var] = (
-                prev_val[var] + self.ode[var](prev_val, self.coeffs) * 0.5 * self.dt
-            )
+            k_2[var] = prev_val[var] + self.ode[var](k_1, self.coeffs) * 0.5 * self.dt
 
         for var in self.var_names:
             k_3[var] = prev_val[var] + self.ode[var](k_2, self.coeffs) * 0.5 * self.dt
@@ -95,21 +99,25 @@ class ODESolver(TimeSeriesModel):
         return pred
 
     def solver(self, nt, initial=None):
-        """Numerical simulation of the ODE using method self.step_solver
+        """Numerical simulation of the ODE using the selected solver method.
+
         Args:
-            nt (int): Number of time-steps.
-            initial (dict): Initial values for each variable.
+            nt (int): Number of time-steps
+            initial (dict): Initial values for each variable
+
         Returns:
-            pred (dict): Prediction of each variable after nt time steps.
+            dict: Prediction of each variable after nt time steps
         """
         if initial is None:
             initial = self.init_vars
+
         pred = {name: value.unsqueeze(0) for name, value in initial.items()}
 
         for n in range(nt - 1):
             # create dictionary containing values from previous time step
             prev_val = {var: pred[var][[n]] for var in self.var_names}
             new_val = self.step_solver(prev_val)
+
             for var in self.var_names:
                 pred[var] = torch.cat([pred[var], new_val[var]])
 
@@ -117,14 +125,14 @@ class ODESolver(TimeSeriesModel):
         return torch.stack([pred[var] for var in self.outvar], dim=1)
 
     def fit(self, x, y, max_epochs=10, batch_size=128):
-        """Fits model to the given data.
+        """Fits the model to the given data.
 
         Args:
             x (torch.Tensor): Input data
             y (torch.Tensor): Output data
             max_epochs (int): Number of training epochs
             batch_size (int): Batch size for torch.utils.data.DataLoader
-                (Set to x.shape[0] if unobserved variables are present)
+                Set to x.shape[0] if unobserved variables are present
         """
         if self.observed:
             super().fit(x, y, max_epochs, batch_size)
